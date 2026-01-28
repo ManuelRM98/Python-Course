29d33561bc61b37fd19db5964aae011134ed91d7
Manuel Rodriguez <manuel98031752@gmail.com>
2026-01-27 22:44:24 -0500

    first commit

---
+++ class4.py
@@ -0,0 +1,31 @@
+name = "MANUEL"
+
+print(type(name))
+
+name1 = 'Carli'
+
+print(type(name1))
+
+name2 = '''Carli
+Kaiser
+'''
+
+print(name2)
+
+#Indexation
+
+print(name[0])
+print(name[-1])
+
+last_name = "Rodriguez Madrid"
+
+print(name + " " + last_name)
+
+print(name * 5)
+
+print(len(last_name))
+print(len(name))
+
+print(name.lower())
+print(last_name.upper())
+print(last_name.strip())
---
+++ class5.py
@@ -0,0 +1,18 @@
+x = 10
+print(type(x))
+y = 5.678
+print(type(y))
+# z = 1.2e6
+# # can also be used with uppercase: 1.2E6
+# print(z)
+# a = 1.2e-6
+# print(a)
+
+print(x+x)
+print(x+y)
+print(y+y)
+
+is_true = True
+is_false = False
+
+print(type(is_true))
