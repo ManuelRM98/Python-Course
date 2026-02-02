24b714479956e96357d048e33ff11a82a2cb0ac3
Manuel Rodriguez <manuel.rodriguez98@proton.me>
2026-02-01 21:03:31 -0500

    learning a new class about numerical operators

---
+++ class6.py
@@ -0,0 +1,42 @@
+#Numerical Operators
+a = 10
+b = 3
+
+print("Sum: ",a+b)
+print("Substract: ",a-b)
+print("Multiplication: ",a*b)
+print("Division: ",a/b)
+print("Exponentiation: ",a**b)
+print("Enteral part of division: ",a//b)
+
+print("Module: ",a%b)
+
+#Shorcut
+
+a += 2
+# a -= 2
+# a *= 2
+# a /= 2
+
+print(a)
+
+operation_1 = 2+3*4
+print(operation_1)
+
+operation_2 = (2+3)*4
+print(operation_2)
+
+operation_3 = (2+3) * (4**2)/8-1
+print(operation_3)
+
+#boolean
+
+a = 10
+b = 3
+
+print(a < b)
+print(a > b)
+print(a <= b)
+print(a >= b)
+print(a == b)
+print(a != b)
