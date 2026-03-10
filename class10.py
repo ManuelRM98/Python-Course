bf0fa19bafe7de26d95fed90580bb72166eef2f6
Manuel Rodriguez <manuel98031752@gmail.com>
2026-03-10 15:55:25 -0500

    Learning about dictionaries in class10

---
+++ class10.py
@@ -0,0 +1,32 @@
+numbers = {1:"one", 2:"two", 3:"three"}
+
+print(numbers)
+
+#To look for something in the dictionary
+
+print(numbers[2])
+
+information = {"name": "Manuel", "last name": "Rodriguez", "height":"1.86", "age":"28"}
+
+del information["age"]
+print(information)
+
+#methods of dictionaries
+
+keys_dict = information.keys()
+print(keys_dict)
+print(type(keys_dict))
+
+value_dict = information.values()
+print(value_dict)
+
+pairs = information.items()
+print(pairs)
+
+#You can create a dictionary of dictionaries
+
+contacts = {"Manuel":{"last name": "Rodriguez", "height":"1.86", "age":"28"},
+            "Mateo":{"last name": "Ramirez", "height":"1.75", "age":"22"}}
+
+print(contacts["Manuel"])
+print(contacts["Mateo"])
